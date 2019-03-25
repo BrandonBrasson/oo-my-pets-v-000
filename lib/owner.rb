7a8cd89e8d85d041debf3746b766bb2e808e81a8
Brandon Brasson <rocksong622@gmail.com>
2019-03-25 15:00:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+    attr_reader :species
+
+    @@owners = []
+
+    def initialize(species, pets = {:fishes => [], :dogs => [], :cats => []})
+      @species = species
+      @pets = pets
+      @@owners << self
+    end
+
+    def say_species
+      "I am a #{@species}."
+    end
 end
